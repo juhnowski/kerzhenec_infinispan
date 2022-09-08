fe438a876b9d09b709dfe8c12631597804b64396
Илья Юхновский <ilya@MacBook-Pro-Ila.local>
2022-09-08 04:49:25 +0300

    change readme

--- adoc.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-#Generate diagrams
-asciidoctor -r asciidoctor-diagram -a generate-diagrams README.adoc
-
-#Generate HTML
-asciidoctor -a source-highlighter=pygments README.adoc
